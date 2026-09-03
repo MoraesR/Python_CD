8389734329ee30b280dd7e415c627c8f523cfb96
Rodrigo Moraes <rodrigomoraes174@gmail.com>
2026-09-02 21:46:31 -0400

    exe3

--- exe3.py
+++ exe3.py
@@ -6,3 +6,12 @@ Se o nível for menor ou igual a 16.0 metros, imprima: "Alerta de Seca".
 Caso contrário, imprima: "Nível Normal".
 """
 
+niv_rio = float(input("Digite o nível atual do rio em metros:  "))
+
+if niv_rio >= 29:
+    print("Alerta de Cheia")
+elif niv_rio <= 16:
+    print("Alerta de Seca")
+else:
+    print("Normal")
+
